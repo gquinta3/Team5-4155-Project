2b9063766a24386527bc83fd4228724a64e130c9
tnguy231-TN <71344519+tnguy231-TN@users.noreply.github.com>
2021-03-29 17:26:07 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { View } from 'react-native';
-import Launch from './LittleFriendApp/screen/Launch';
+import { View, Text, Button } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigation } from 'react-navigation-stack';
+
+import Home from './LittleFriendApp/screen/Home';
+import SignUp from './LittleFriendApp/screen/SignUp';
+import LogIn from './LittleFriendApp/screen/LogIn';
+import Welcome from './LittleFriendApp/screen/Welcome';
 
 export default function App() {
-  return <Launch />;
+  return  <Welcome />;
 }
-
-
